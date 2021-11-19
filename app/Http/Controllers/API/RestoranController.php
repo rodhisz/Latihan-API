a2222743a9060379525d21e8aa270b765a1aa974
rodhisz <rodhisz.45@gmail.com>
2021-11-19 14:40:38 +0700

    commit 7

--- app/Http/Controllers/API/RestoranController.php
+++ app/Http/Controllers/API/RestoranController.php
@@ -36,6 +36,7 @@ class RestoranController extends Controller
         $resto->rating     = $request->rating;
         $resto->save();
 
+        //Tambah menu satu satu
         // $menu      = new Menu();
 
         // $menu->resto_id       = $resto->id;
@@ -44,6 +45,7 @@ class RestoranController extends Controller
         // $menu->nama_kategori  = $request->nama_kategori;
         // $menu->save();
 
+        //Tambah menu langsung banyak
         foreach($request->list_menu as $value)
         {
             $menu = array(
@@ -61,7 +63,7 @@ class RestoranController extends Controller
             'status'   => 1,
             'pesan'    => "Menu Berhasil Ditambahkan!",
             'Resto'    => $resto,
-            'Data'     => $data
+            'Menu'     => $data
         ],Response::HTTP_OK);
     }
 
@@ -127,6 +129,170 @@ class RestoranController extends Controller
             'pesan'    => "Data Resto Kamu Berhasil Diupdate",
             'data'     => $resto
         ],Response::HTTP_OK);
+    }
+
+    public function searchMenu(Request $request)
+    {
+        $keyword = $request->search;
+
+        //Jika menu tidak ada
+        $menuempty =  Menu::where('nama_menu','like',"%". $keyword . "%")->orWhere('nama_kategori','like',"%". $keyword . "%")->first();
+
+        if(!$menuempty) {
+            return $this->responError(0, "Hasil Pencarian '$keyword' Tidak ada");
+        }
+
+        //Jika menu ada
+        $menu =  Menu::where('nama_menu','like',"%". $keyword . "%")->orWhere('nama_kategori','like',"%". $keyword . "%")->get();
+
+        return response()->json([
+            'status'   => 1,
+            'pesan'     => "Hasil Pencarian '$keyword'",
+            'result'     => $menu
+        ],Response::HTTP_OK);
+    }
+
+    public function deleteMenu($resto_id, $menu_id)
+    {
+        $resto = Restoran::findOrFail($resto_id)->first();
+
+        if(!$resto){
+            return $this->responError(0, "Resto ID Tidak Ditemukan");
+        }
+
+        $menu = Menu::findOrFail($menu_id);
+
+        if(!$menu){
+            return $this->responError(0, "Menu ID Tidak Ditemukan");
+        }
+
+        $menu->delete();
+
+        return response()->json([
+            'status'   => 1,
+            'pesan'    => "'$menu->nama_menu' Berhasil Dihapus dari $resto->nama_resto",
+        ],Response::HTTP_OK);
+    }
+
+    public function createMenu(Request $request, $resto_id)
+    {
+        $resto = Restoran::find($resto_id);
+
+        if(!$resto){
+            return $this->responError(0, "Resto ID Tidak Ditemukan");
+        }
+
+        $request-> validate([
+            'nama_menu'     => 'required',
+            'harga'         => 'required',
+            'nama_kategori' => 'required',
+        ]);
+
+        $menu = new Menu();
+
+        $menu->resto_id      = $resto->id;
+        $menu->nama_menu     = $request->nama_menu;
+        $menu->harga         = $request->harga;
+        $menu->nama_kategori = $request->nama_kategori;
+        $menu->save();
+
+        return response()->json([
+            'status'   => 1,
+            'pesan'    => "Berhasil menambahkan menu '$menu->nama_menu' ke $resto->nama_resto",
+            'result'   => $menu
+        ],Response::HTTP_OK);
+    }
+
+    public function editMenu(Request $request, $resto_id, $menu_id)
+    {
+        $resto = Restoran::find($resto_id);
+
+        if(!$resto){
+            return $this->responError(0, "Resto ID Tidak Ditemukan");
+        }
+
+        $menu = Menu::find($menu_id);
+
+        if(!$menu){
+            return $this->responError(0, "Menu ID Tidak Ditemukan");
+        }
+
+        $request-> validate([
+            'nama_menu'     => 'required',
+            'harga'         => 'required',
+            'nama_kategori' => 'required',
+        ]);
+
+        $menu->update([
+            'nama_menu'     =>  $request->nama_menu,
+            'harga'         =>  $request->harga,
+            'nama_kategori' =>  $request->nama_kategori,
+        ]);
+
+        return response()->json([
+            'status'   => 1,
+            'pesan'    => "Berhasil Merubah Data Menu '$request->nama_menu' dari $resto->nama_resto",
+            'data'     => $menu
+        ],Response::HTTP_OK);
+    }
+
+    public function updateRestoMenu(Request $request, $resto_id)
+    {
+        $resto = Restoran::find($resto_id);
+
+        if(!$resto){
+            return $this->responError(0, "Resto ID Tidak Ditemukan");
+        }
+
+        $request-> validate([
+            'nama_resto' => 'required',
+            'alamat'     => 'required',
+            'telp'       => 'required',
+            'jam_buka'   => 'required',
+        ]);
+
+        $resto->update([
+            'nama_resto' =>  $request->nama_resto,
+            'alamat'     =>  $request->alamat,
+            'telp'       =>  $request->telp,
+            'jam_buka'   =>  $request->jam_buka,
+            'rating'     =>  $request->rating
+        ]);
+
+        Menu::where('resto_id', $resto->id)->delete();
+
+        foreach($request->list_menu as $value)
+        {
+            $menu = array(
+                'resto_id'      => $resto->id,
+                'nama_menu'     => $value['nama_menu'],
+                'harga'         => $value['harga'],
+                'nama_kategori' => $value['nama_kategori'],
+            );
+            Menu::create($menu);
+        }
+
+        $data = Menu::where('resto_id', $resto->id)->get();
+
+        return response()->json([
+            'status'   => 1,
+            'pesan'    => "Menu Upadete Menu Baru di $resto->nama_resto!",
+            'Resto'    => $resto,
+            'Menu'     => $data
+        ],Response::HTTP_OK);
+
+    }
+
+    public function getAllResto()
+    {
+        $resto = Restoran::all();
+
+        return response()->json([
+            'status'    => 1,
+            'message'   => "Berhasil Mendapatkan Semua Resto",
+            'result'    => $resto
+        ], Response::HTTP_NOT_FOUND);
+
     }
 
     public function responError($sts, $pesan)
--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,32 @@ Route::put('/edit/{id}', [AuthController::class, 'editProfile']);
 Route::put('/editpassword/{id}', [AuthController::class, 'editPassword']);
 
 
-//-->CRUD Resto dan Menu nya<--
+//CRUD Resto dan Menu nya
 Route::post('/add/resto-dan-menu', [RestoranController::class, 'createRestoMenu']);
 Route::get('/resto/{id}', [RestoranController::class, 'getRestoMenu']);
 
 //Get semua menu
 Route::get('/allmenu', [RestoranController::class, 'getAllMenu']);
 
+//Edit Resto
 Route::put('/editresto/{id}', [RestoranController::class, 'editResto']);
 
+//Searching Menu
+Route::get('/search', [RestoranController::class, 'searchMenu']);
+
+//Delete Menu
+Route::delete('/delete/menu/{resto_id}/{menu_id}', [RestoranController::class, 'deleteMenu']);
+
+//Bikin 1 Menu ke resto
+Route::post('/add/menu/{resto_id}', [RestoranController::class, 'createMenu']);
+
+//Edit 1 menu di resto
+Route::put('/edit/menu/{resto_id}/{menu_id}', [RestoranController::class, 'editMenu']);
+
+//edit banyak menu di resto
+Route::put('/update/restomenu/{resto_id}', [RestoranController::class, 'updateRestoMenu']);
+
+//Get semua menu
+Route::get('/allresto', [RestoranController::class, 'getAllResto']);
 
 
